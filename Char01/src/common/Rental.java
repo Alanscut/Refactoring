97022aec64a8c383b5583e8958aa3de3f09a3705
Alanscut <wp_scut@163.com>
2019-11-03 20:44:00 +0800

    refactoring first demo

---
+++ Char01/src/common/Rental.java
@@ -0,0 +1,22 @@
+package common;
+
+import common.Movie;
+
+public class Rental {
+
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+}
