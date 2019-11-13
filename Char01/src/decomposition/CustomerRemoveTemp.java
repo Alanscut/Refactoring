798482a5b1aa26f198448ddc55a5bc24928329ef
Alanscut <wp_scut@163.com>
2019-11-13 22:57:42 +0800

    remove tmp variable

---
+++ Char01/src/decomposition/CustomerRemoveTemp.java
@@ -0,0 +1,138 @@
+package decomposition;
+
+import common.Movie;
+import common.Rental;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class CustomerRemoveTemp {
+    private String name;
+    private List<Rental> list = new ArrayList<>();
+
+    public CustomerRemoveTemp(String name) {
+        this.name = name;
+    }
+
+    public void addRental(Rental rental) {
+        list.add(rental);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /*
+     * too many business in this function
+     */
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Iterator<Rental> rentals = list.iterator();
+        String result = "common.Rental Record for " + getName() + "\n";
+        while (rentals.hasNext()) {
+//            double thisAmount = 0;  // remove temp variable
+            Rental each = rentals.next();
+
+            /**************************************************/
+            /*
+            // determine amounts for each line
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2) {
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    }
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3) {
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    }
+                    break;
+            }
+            */
+
+//            thisAmount = amountFor(each);
+//            thisAmount = each.getCharge();
+            /**************************************************/
+
+            // add frequent renter points
+            frequentRenterPoints ++;
+            // add bonus for a two day new release rental
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
+                frequentRenterPoints++;
+            }
+
+            //show figures for this rental
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
+        }
+
+        // add footer lines
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return result;
+    }
+
+    /*private double amountFor(Rental each) {
+        int thisAmount = 0;
+        switch (each.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (each.getDaysRented() > 2) {
+                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (each.getDaysRented() > 3) {
+                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
+    }*/
+
+    /**
+     *  rename the local variable
+     *  excellent code should express its feature clearly by itself.
+     *  naming the variable is the key point of clear code.
+     * @param rental
+     * @return
+     */
+    // move this to Rental
+    /*private double amountFor(Rental rental) {
+        int result = 0;
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (rental.getDaysRented() > 2) {
+                    result += (rental.getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (rental.getDaysRented() > 3) {
+                    result += (rental.getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }*/
+
+    private double amountFor(Rental rental) {
+        return rental.getCharge();
+    }
+}
